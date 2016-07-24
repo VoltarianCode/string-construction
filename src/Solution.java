eb2aa7c1a1ba6d4c598bead4e986fc1dd5bda9aa
VoltarianCode <illya.semenkov@gmail.com>
2016-07-24 03:48:39 -0400

    using HashSet of characters which were already seen, to build p one character at a time

--- src/Solution.java
+++ src/Solution.java
@@ -4,36 +4,8 @@
 
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-    /*
-    public static ArrayList getSubstrings(String p){
-        ArrayList <String> substrings  = new ArrayList<String>();
-        substrings.add(p);
-
-        for (int i = p.length() - 1; i > 0; i--){
-            for (int j = 0; (j+i) < p.length(); j++){
-                substrings.add(p.substring(j , j+i));
-            }
-        }
-        return substrings;
-    }
-    */
-    public static HashSet<String> getSubstrings(String str) {
-        HashSet<String> substrings = new HashSet<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j <= i; j++) {
-                if (!substrings.contains(str.substring(j, i + 1))) {
-                    substrings.add(str.substring(j, i + 1));
-                }
-            }
-        }
-        return substrings;
-    }
 
     public static void main(String[] args) {
         Scanner in = null;
@@ -42,6 +14,8 @@ public class Solution {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //long start = System.currentTimeMillis();
+
         int n = in.nextInt();
         for (int a0 = 0; a0 < n; a0++) {
             String s = in.next();
@@ -49,39 +23,34 @@ public class Solution {
             int s_length = s.length();
             int progress = 0;
             int totalCost = 0;
-            boolean found = false;
-            char array[] = s.toCharArray();
+            HashSet<Character> reference = new HashSet<>();
+            char s_array[] = s.toCharArray();
             while (progress != s_length) {
 
                 if (progress == 0) {
-                    p += array[0];
+                    p += s_array[0];
+                    reference.add(s_array[0]);
                     totalCost++;
                     progress++;
                 } else {
-                    HashSet<String> substrings = getSubstrings(p);
-
-                    outerloop:
-                    for (String str : substrings) {
-                        if (s.indexOf(str, progress) == progress) {
-                            p += str;
-                            progress += str.length();
-                            found = true;
-                            break outerloop;
-                        } else {
-                            found = false;
-                        }
-                    }
-
-
-                    if (!found) {
-                        p += array[progress];
+                    if (reference.contains(s_array[progress])) {
+                        p += s_array[progress];
+                        progress++;
+                    } else {
+                        p += s_array[progress];
+                        reference.add(s_array[progress]);
                         totalCost++;
                         progress++;
                     }
 
                 }
             }
+
             System.out.println(totalCost);
+
         }
+       // long end = System.currentTimeMillis();
+       // System.out.println("Running time: " + (end - start));
     }
+
 }
